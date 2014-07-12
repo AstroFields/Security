9882bc1a22ebac7ca6d78317aa3f8ecaf708d693
Kaiser Franz Josef <wecodemore@gmail.com>
2014-07-12 17:29:04 +0200

    Initial commit to GitHub

---
+++ Bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace WCM;
+
+/**
+ * Plugin Name: (WCM) AstroFields Security
+ * Description: Security/Sanitize Commands Extension for the AstroFields framework
+ */
+
+// Drop in Composer autoloader
+require_once plugin_dir_path( __FILE__ )."vendor/autoload.php";
